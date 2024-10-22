a7b7a889b4ed0d5fbc7ce8d50f461854ea25b809
Meher Mankar <84778567+MeherMankar@users.noreply.github.com>
2024-10-22 22:02:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // script.js
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    const backToTopButton = document.getElementById("backToTop");
+    
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+}
 
 function topFunction() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
