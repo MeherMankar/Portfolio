3bc14cab5a253f58d417093921fc48c7ef17a9a4
Meher Mankar <84778567+MeherMankar@users.noreply.github.com>
2024-11-11 17:12:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -15,3 +14,21 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
+
+// Function to change background image
+function changeBackgroundImage() {
+    const images = [
+        'background1.jpg',
+        'background2.jpg',
+        'background3.jpg',
+        'background4.jpg',
+        'background5.jpg'
+    ]; // Array of image URLs
+    const randomImage = images[Math.floor(Math.random() * images.length)]; // Pick a random image
+    document.body.style.backgroundImage = url('${randomImage}'); // Set the background image
+}
+
+// Call the function on page load
+window.onload = function() {
+    changeBackgroundImage();
+};
